e497d50b1c66e80f7cd0f8a7f928b08413bb471c
nhiempc <nhiem10596@gmail.com>
2023-02-22 18:06:17 +0700

    create server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './schema/schema.js';
+import connectDB from './config/db.js';
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: process.env.NODE_ENV === 'development'
+    })
+);
+
+app.listen(port, console.log(`Server is running on port ${port}`));
