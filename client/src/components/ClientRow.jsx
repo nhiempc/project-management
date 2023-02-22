410735beda19b184bcc43cc45b6654429d858f5b
nhiempc <nhiem10596@gmail.com>
2023-02-22 23:51:34 +0700

    delete client mutation

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,6 +1,25 @@
 import { FaTrash } from 'react-icons/fa';
+import { useMutation } from '@apollo/client';
+import { DELETE_CLIENT } from '../mutations/clientMutations';
+import { GET_CLIENTS } from '../queries/clientQueries';
 
 export default function ClientRow({ client }) {
+    const [deleteClient] = useMutation(DELETE_CLIENT, {
+        variables: {
+            id: client.id
+        },
+        refetchQueries: [{ query: GET_CLIENTS }]
+        // Refetch Or using update
+        // update(cache, { data: { deleteClient } }) {
+        //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
+        //   cache.writeQuery({
+        //     query: GET_CLIENTS,
+        //     data: {
+        //       clients: clients.filter((client) => client.id !== deleteClient.id),
+        //     },
+        //   });
+        // },
+    });
     return (
         <>
             <tr>
@@ -8,7 +27,10 @@ export default function ClientRow({ client }) {
                 <td>{client.email}</td>
                 <td>{client.phone}</td>
                 <td>
-                    <button className='btn btn-danger btn-sm'>
+                    <button
+                        className='btn btn-danger btn-sm'
+                        onClick={deleteClient}
+                    >
                         <FaTrash />
                     </button>
                 </td>
